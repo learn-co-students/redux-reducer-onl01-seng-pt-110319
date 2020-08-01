87c1002d287d03a15ae363de2729bef1cd26eb6a
Collin Jilbert <cjilbert504@gmail.com>
2020-08-01 12:32:50 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+    console.log(action.friend);
+    if (action.type === "ADD_FRIEND") {
+        return { ...state, friends: [ ...state.friends, action.friend ] }
+    } else if (action.type === "REMOVE_FRIEND") {
+        return { ...state, friends: state.friends.filter(friend => friend.id !== action.id )}
+    }
+
+    return state;
 }
