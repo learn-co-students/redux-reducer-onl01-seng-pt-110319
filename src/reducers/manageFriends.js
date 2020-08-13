b3d73523437a65406d33273ea126321ae3359333
Jordan <jordanleelong5@gmail.com>
2020-08-12 21:19:11 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,66 @@
-export function manageFriends(state, action){
-}
+// export function manageFriends(state, action){
+//     state = {friends: []}
+
+
+//     switch(action.type) {
+//         case 'ADD_FRIEND':
+//             return (
+//                 {...state,
+//                     friends: [
+//                       ...state.friends,
+//                       action.friend
+//                     ]
+//                 }
+//             )
+//             case 'REMOVE_FRIEND': 
+//             // FIND THE FRIEND THAT ID MATCHES AND REMOVE THEM. 
+//             //RETURN NEW STATE WITH ARRAY FRIENDS THAT INCLUDES EVERYONE EXCEPT THE REMOVED FRIEND
+//                 const removeFriend = state.friends.findIndex(friend => friend.id === action.id)
+//                 return (
+//                     {...state, 
+//                     friends: [
+//                         ...state.friends, 
+//                         state.friends - removeFriend
+//                     ]
+//                 }
+
+//                 )
+//                 default:
+//                     return state;
+                    
+//     }
+// }
+
+  
+export function manageFriends(state = {
+    friends: [],
+  }, action) {
+    switch(action.type) {
+  
+      case "ADD_FRIEND":
+        return (
+          {...state,
+              friends: [
+                ...state.friends,
+                action.friend
+              ]
+          }
+      )
+      case 'REMOVE_FRIEND':
+         // iterate over state.friends find the id that matches action id
+         // remove that friend from the list and return state.friends
+         const friendRemoval = state.friends.findIndex(friend => friend.id === action.id)
+         return (
+            {...state, 
+                friends: [
+                    ...state.friends.slice(0, friendRemoval),
+                    ...state.friends.slice(friendRemoval + 1)
+
+                    
+                ]
+        }
+         )
+          default: 
+          return state
+        }
+    }
